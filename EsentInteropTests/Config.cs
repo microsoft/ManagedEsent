190368a2976cf9443f02ab3561062cf1f21e9e83
laurionb_cp <SND\laurionb_cp>
2009-02-05 15:34:48 -0800

    Add missing config file.

---
+++ EsentInteropTests/Config.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Config.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace InteropApiTests
+{
+    using System;
+
+    /// <summary>
+    /// Esent configuration information.
+    /// </summary>
+    public static class Config
+    {
+        public static bool SupportsVistaFeatures
+        {
+            get
+            {
+                return Environment.OSVersion.Version.Major >= 6;
+            }
+        }
+    }
+}
