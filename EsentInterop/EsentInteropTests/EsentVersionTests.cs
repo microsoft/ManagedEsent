05be7cdb15016e362d285b4e3373a9b188a644a6
laurionb_cp <SND\laurionb_cp>
2009-05-07 20:20:16 -0700

    Add/Remove files for EsentVersion

---
+++ EsentInterop/EsentInteropTests/EsentVersionTests.cs
@@ -0,0 +1,59 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EsentVersionTests.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Microsoft.Isam.Esent.Interop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InteropApiTests
+{
+    /// <summary>
+    /// Test the static version class
+    /// </summary>
+    [TestClass]
+    public class EsentVersionTests
+    {
+        /// <summary>
+        /// Print the current version of Esent (for debugging)
+        /// </summary>
+        [TestMethod]
+        [Priority(0)]
+        public void PrintVersion()
+        {
+            Console.WriteLine("{0}.{1}", EsentVersion.MajorVersion, EsentVersion.BuildNumber);
+
+            if (EsentVersion.SupportsVistaFeatures)
+            {
+                Console.WriteLine("SupportsVistaFeatures");
+            }
+
+            if (EsentVersion.SupportsWindows7Features)
+            {
+                Console.WriteLine("SupportsWindows7Features");
+            }
+        }
+
+        /// <summary>
+        /// Verify the major version is not zero.
+        /// </summary>
+        [TestMethod]
+        [Priority(0)]
+        public void VerifyMajorVersionIsNotZero()
+        {
+            Assert.AreNotEqual(0, EsentVersion.MajorVersion);
+        }
+
+        /// <summary>
+        /// Verify the build number is not zero.
+        /// </summary>
+        [TestMethod]
+        [Priority(0)]
+        public void VerifyBuildNumberIsNotZero()
+        {
+            Assert.AreNotEqual(0, EsentVersion.BuildNumber);
+        }
+    }
+}
---
+++ EsentInterop/EsentVersion.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EsentVersion.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Isam.Esent.Interop
+{
+    /// <summary>
+    /// Gives information about the version of esent being used.
+    /// </summary>
+    public static class EsentVersion
+    {
+        /// <summary>
+        /// Initializes static members of the Version class.
+        /// </summary>
+        static EsentVersion()
+        {
+            var version = (uint)GetVersionFromEsent();
+            MajorVersion = (int)(version >> 28);
+            BuildNumber = (int) ((version & 0xFFFFFF) >> 8);
+
+            if (BuildNumber >= 6000)
+            {
+                SupportsVistaFeatures = true;
+            }
+
+            if (BuildNumber >= 7000)
+            {
+                SupportsWindows7Features = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets the major version of esent
+        /// </summary>
+        public static int MajorVersion { get; internal set; }
+
+        /// <summary>
+        /// Gets the build number of esent
+        /// </summary>
+        public static int BuildNumber { get; internal set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the current version of esent
+        /// supports features available in the Windows Vista version of
+        /// esent.
+        /// </summary>
+        public static bool SupportsVistaFeatures { get; internal set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the current version of esent
+        /// supports features available in the Windows 7 version of
+        /// esent.
+        /// </summary>
+        public static bool SupportsWindows7Features { get; internal set; }
+
+        /// <summary>
+        /// Create an instance and get the current version of Esent.
+        /// </summary>
+        /// <returns>The current version of Esent.</returns>
+        private static int GetVersionFromEsent()
+        {
+            using (var instance = new Instance("GettingEsentVersion"))
+            {
+                instance.Parameters.Recovery = false;
+                instance.Parameters.NoInformationEvent = true;
+                instance.Parameters.MaxTemporaryTables = 0;
+                instance.Init();
+
+                using (var session = new Session(instance))
+                {
+                    int version;
+                    Api.JetGetVersion(session, out version);
+                    return version;
+                }
+            }
+        }
+    }
+}
