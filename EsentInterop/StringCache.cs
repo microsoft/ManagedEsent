aa5061bc1d376c44380b9082333e1a7d41d90571
laurionb_cp <SND\laurionb_cp>
2010-08-08 16:06:14 -0700

    Add missing files.

---
+++ EsentInterop/StringCache.cs
@@ -0,0 +1,66 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringCache.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Isam.Esent.Interop
+{
+    using System;
+
+    /// <summary>
+    /// Class that helps cache strings.
+    /// </summary>
+    internal static class StringCache
+    {
+        /// <summary>
+        /// Return the interned version of a string, or the original
+        /// string if it isn't interned.
+        /// </summary>
+        /// <param name="s">The string to try to intern.</param>
+        /// <returns>An interned copy of the string or the original string.</returns>
+        public static string TryToIntern(string s)
+        {
+            return String.IsInterned(s) ?? s;
+        }
+
+        /// <summary>
+        /// Convert a byte array to a string.
+        /// </summary>
+        /// <param name="value">The bytes to convert.</param>
+        /// <param name="startIndex">The starting index of the data to convert.</param>
+        /// <param name="count">The number of bytes to convert.</param>
+        /// <returns>A string converted from the data.</returns>
+        public static string GetString(byte[] value, int startIndex, int count)
+        {
+            unsafe
+            {
+                fixed (byte* data = value)
+                {
+                    char* chars = (char*)(data + startIndex);
+                    return GetString(chars, 0, count / sizeof(char));
+                }
+            }            
+        }
+
+        /// <summary>
+        /// Convert a char array to a string.
+        /// </summary>
+        /// <param name="value">The characters to convert.</param>
+        /// <param name="startIndex">The starting index of the data to convert.</param>
+        /// <param name="count">The number of characters to convert.</param>
+        /// <returns>A string converted from the data.</returns>
+        public static unsafe string GetString(char* value, int startIndex, int count)
+        {
+            if (0 == count)
+            {
+                return String.Empty;
+            }
+
+            // Encoding.Unicode.GetString copies the data to an array of chars and then
+            // makes a string from it, copying the data twice. Use the more efficient
+            // char* constructor.
+            return new string(value, startIndex, count);
+        }
+    }
+}
---
+++ EsentInteropTests/StringCacheTests.cs
@@ -0,0 +1,68 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StringCacheTests.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace InteropApiTests
+{
+    using System;
+    using System.Text;
+    using Microsoft.Isam.Esent.Interop;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Test the StringCache class.
+    /// </summary>
+    [TestClass]
+    public class StringCacheTests
+    {
+        /// <summary>
+        /// Try to intern a random string (should not be interned).
+        /// </summary>
+        [TestMethod]
+        [Description("Try to intern a random string (should not be interned)")]
+        [Priority(0)]
+        public void TryToInternRandomString()
+        {
+            string s = StringCache.TryToIntern(Any.String);
+            Assert.IsNull(String.IsInterned(s), "Should not have been interned");
+        }
+
+        /// <summary>
+        /// Try to intern an interned string.
+        /// </summary>
+        [TestMethod]
+        [Description("Try to intern an interned string")]
+        [Priority(0)]
+        public void TryToInternInternedString()
+        {
+            string s = String.Intern(StringCache.TryToIntern(Any.String));
+            Assert.IsNotNull(String.IsInterned(s), "Should not have been interned");
+        }
+
+        /// <summary>
+        /// Get a string with a null buffer.
+        /// </summary>
+        [TestMethod]
+        [Description("Test StringCache.GetString with a null buffer")]
+        [Priority(0)]
+        public void GetStringWithNull()
+        {
+            byte[] buffer = null;
+            Assert.AreEqual(String.Empty, StringCache.GetString(buffer, 0, 0));
+        }
+
+        /// <summary>
+        /// Get a string with a null buffer.
+        /// </summary>
+        [TestMethod]
+        [Description("Test StringCache.GetString")]
+        [Priority(0)]
+        public void GetString()
+        {
+            byte[] buffer = Encoding.Unicode.GetBytes("Hello");
+            Assert.AreEqual("Hello", StringCache.GetString(buffer, 0, buffer.Length));
+        }
+    }
+}
