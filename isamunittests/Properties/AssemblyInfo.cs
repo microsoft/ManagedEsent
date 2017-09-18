54338ce42e6883641898ad98179432e99c55bab4
Michael Thorp <mthorp@microsoft.com>
2017-09-18 15:35:56 -0700

    Setting assemblies to 1.9.5 version

--- isamunittests/Properties/AssemblyInfo.cs
+++ isamunittests/Properties/AssemblyInfo.cs
@@ -17,6 +17,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IsamUnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("IsamUnitTests")]
+[assembly: AssemblyCopyright("Copyright (c) Microsoft. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -24,3 +28,16 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
