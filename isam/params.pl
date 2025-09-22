749408bf0c32d065903956ad3296f5523ff3ac35
Michael Thorp <mthorp@microsoft.com>
2025-09-22 14:41:07 -0700

    Isam is sync'd

--- isam/params.pl
+++ isam/params.pl
@@ -1,4 +1,5 @@
-# Copyright (c) Microsoft Corporation.  All rights reserved.
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 #
 # Usage:
 #   Do not use, go to the ese dev root, and call gengen.bat
